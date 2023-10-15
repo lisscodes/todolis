4328d0d15edd2bb0a5ba5adf01c2ff67e6b36a1b
lisscodes <lisandra.dias@jala.university>
2023-10-15 11:54:07 -0300

    first commit

---
+++ src/main/java/br/com/lisandradias/todolis/TodolisApplication.java
@@ -0,0 +1,13 @@
+package br.com.lisandradias.todolis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lisandradias/todolis/TodolisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lisandradias.todolis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
